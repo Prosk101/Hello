2e8710aaa8e17e015862874728108f7beb47eff7
jerwin21 <46761152+jerwin21@users.noreply.github.com>
2019-01-17 10:20:18 -0600

    Update hello.c
    added my name and my major

--- hello.c
+++ hello.c
@@ -4,6 +4,8 @@ int main(int argc, char **argv){
 
   printf("Samuel C Probst\n")
   printf("Electrical Engineering\n");
+  printf("John W Erwin\n");
+  printf("Computer Science");
   
   return 0;
 }
